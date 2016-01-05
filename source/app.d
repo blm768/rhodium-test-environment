4d993f00024dc8ec59da453d0bab289adc22ac3c
Benjamin L. Merritt <blm768@gmail.com>
2016-01-04 23:23:18 -0800

    Testing for unsigned int literals

--- source/app.d
+++ source/app.d
@@ -2,12 +2,19 @@ module main;
 
 import std.stdio;
 
+import rhodium.ast;
 import rhodium.lexer;
+import rhodium.value;
+import rhodium.value.integer;
 
 void main() {
 	auto source = new SourceText("abc 123");
 	auto lexer = Lexer(source);
 	foreach(Token t; lexer) {
-		writeln(t);
+		if(t.type == TokenType.integerLiteral) {
+			auto literal = new IntegerLiteral(t);
+			ValueRef!UnsignedIntegerValue value = new UnsignedIntegerValue(literal);
+			writeln(value.toUlong);
+		}
 	}
 }
