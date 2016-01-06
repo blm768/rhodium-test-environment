4eefd9f3ee8bc1b733d85f44229aaeeaa09ec666
Benjamin L. Merritt <blm768@gmail.com>
2016-01-05 22:26:42 -0800

    Started testing for parser

--- source/app.d
+++ source/app.d
@@ -4,17 +4,14 @@ import std.stdio;
 
 import rhodium.ast;
 import rhodium.lexer;
+import rhodium.parser;
 import rhodium.value;
 import rhodium.value.integer;
 
 void main() {
-	auto source = new SourceText("abc 123");
+	auto source = new SourceText("123 abc 123");
 	auto lexer = Lexer(source);
-	foreach(Token t; lexer) {
-		if(t.type == TokenType.integerLiteral) {
-			auto literal = new IntegerLiteral(t);
-			ValueRef!UnsignedIntegerValue value = new UnsignedIntegerValue(literal);
-			writeln(value.toUlong);
-		}
-	}
+	auto literal = parseIntegerLiteral(lexer);
+	ValueRef!UnsignedIntegerValue value = new UnsignedIntegerValue(literal);
+	writeln(value.toUlong);
 }
